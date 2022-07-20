f8d4921122725d854437af3486d9fdaf54b45241
wenjie <wenjieb2@gmail.com>
2022-07-20 21:09:32 +0800

    Use for loop to create divs in a column

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const container = document.querySelector(".container");
 let row = document.createElement("div");
 row.classList.add('row');
 container.appendChild(row);
+
+for (i = 0; i <= size-1; i++) {
+    let div = document.createElement("div");
+    row.appendChild(div);
+}

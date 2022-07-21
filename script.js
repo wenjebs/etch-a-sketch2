b7687b860c6d546bbd2f9c9de0f39949860e28e5
wenjie <wenjieb2@gmail.com>
2022-07-21 18:59:52 +0800

    Add eventlisteners for box

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let size = 16;
 
 const container = document.querySelector(".container");
+container.setAttribute('draggable',false);
 
 
 createContainer()
@@ -10,17 +11,20 @@ createContainer()
 function createContainer() {
     //initilisation column
     let row = document.createElement("div");
+    row.setAttribute('draggable',false);
     row.classList.add('row');
     container.appendChild(row);
 
     for (i = 0; i <= size-1; i++) {
         let div = document.createElement("div");
         div.classList.add('box');
+        div.setAttribute('draggable',false);
         row.appendChild(div);
     }
     
     for (i = 0; i <= size-2; i++) {
         let row = document.querySelector(".row");
+        row.setAttribute('draggable',false);
         container.appendChild(row.cloneNode(true));
     }
     //add hover event toeverybox for every new box created
@@ -34,10 +38,21 @@ function addHoverEvent() {
     let boxNodeList = document.querySelectorAll(".box")
 
     boxNodeList.forEach(box => {
-        box.addEventListener('mouseover', () => {
-            box.style.backgroundColor = 'yellow'
+        //mousedown means cfm over the box
+        box.addEventListener('mousedown', () => {
+            box.style.backgroundColor = 'black'
+        }),   
+        // incase got drag for now cause idk how remove
+        box.addEventListener('dragover', () => {
+            box.style.backgroundColor = 'black'
         })
-    });
+        //  mouseover + check if the left mouse button is clciked
+        box.addEventListener('mouseover', (event) => {
+            if (event.buttons == 1) {
+                event.target.style.backgroundColor = 'black'
+            }
+        });
+    })
 }
 
 // the reset fxn
